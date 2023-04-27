ab008fe3eb6d1cfabaadcd4ca91d440abbbf30a2
Vladyslav Ushakov <vlad.ushakov.977@gmail.com>
2023-04-27 22:32:46 +0300

    Make hw

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// import './styles.css';
+import { theme } from './settings/theme';
 import { App } from 'components/App';
-import './index.css';
+import { ThemeProvider } from '@emotion/react';
+import { GlobalStyle } from 'GlobalStyle';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App title="Phonebook" />
+    </ThemeProvider>
   </React.StrictMode>
 );
